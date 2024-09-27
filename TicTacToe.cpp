9550efc98d9e776370709c315b81c56ce7fe1d6f
U-BSD\472779 <472779@SHS-HYBF6X3.beaverton.k12.or.us>
2024-09-27 09:43:00 -0700

    final v0

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void printBoard(char grid[][3]) {
+  cout << "____________________________________________" << endl;
   cout             <<"  1"         <<"2"         <<"3"   << endl;
   cout << "a " << grid[0][0] << grid[1][0] << grid[2][0] << endl;
   cout << "b " << grid[0][1] << grid[1][1] << grid[2][1] << endl;
@@ -11,19 +12,41 @@ void printBoard(char grid[][3]) {
 }
 
 bool checkWin(char grid[][3], char player, char any, char none) {
-  // win condition library
-  // horizontal win
-  char winH[][3] = {
+  // WIN CONDITION LIBRARY:
+  // horizontal wins:
+  char winH0[][3] = {
+    {any,  any,  any},
+    {none, none, none},
+    {none, none, none}
+  };
+  char winH1[][3] = {
     {none, none, none},
     {any,  any,   any},
     {none, none, none}
   };
-  // vertical win 
-  char winV[][3] = {
+  char winH2[][3] = {
+    {none, none, none},
+    {none, none, none},
+    {any,  any,  any}
+  };
+  
+  // vertical wins: 
+  char winV0[][3] = {
+    {any, none, none},
+    {any, none, none},
+    {any, none, none}
+  };
+  char winV1[][3] = {
     {none, any, none},
     {none, any, none},
     {none, any, none}
   };
+  char winV2[][3] = {
+    {none, none, any},
+    {none, none, any},
+    {none, none, any}
+  };
+  
   // left diagonal win
   char winDL[][3] = {
     {any,  none,  none},
@@ -37,70 +60,73 @@ bool checkWin(char grid[][3], char player, char any, char none) {
     {any,  none, none}
   };
 
-  int hwin = 0;
-  int vwin = 0;
+  int h0win = 0;
+  int h1win = 0;
+  int h2win = 0;
+  int v0win = 0;
+  int v1win = 0;
+  int v2win = 0;
   int dlwin = 0;
   int drwin = 0;
-
-  char debugGrid[][3] {
-    {none, none, none},
-    {none, none, none},
-    {none, none, none}
-  };
-
-  cout << "checking win for " << player << endl;
   
-  // 
+  // check if stuff meets win conditions
   for (int x = 0; x < 3; x++) {
     for (int y = 0; y < 3; y++) {
+      // current value for convenience
       char value = grid[x][y];
 
-      debugGrid[x][y] = '*';
-      
+      // check for wins: 
       if (value == player) {
-	if (winH[x][y] == any) {
-	  hwin++;
-	  debugGrid[x][y] = '-';
+	// check for horizontal wins:
+	if (winH0[x][y] == any) {
+	  h0win++;
 	}
-	if (winV[x][y] == any) {
-	  vwin++;
-	  debugGrid[x][y] = '|';
+	if (winH1[x][y] == any) {
+	  h1win++;
 	}
+	if (winH2[x][y] == any) {
+	  h2win++;
+	}
+
+	// check for vertical wins:
+	if (winV0[x][y] == any) {
+	  v0win++;
+	}
+	if (winV1[x][y] == any) {
+	  v1win++;
+	}
+	if (winV2[x][y] == any) {
+	  v2win++;
+	}
+
+	// check for diagonal left wins:
 	if (winDL[x][y] == any) {
 	  dlwin++;
-	  debugGrid[x][y] = '<';
 	}
+
+	// check for diagonal right wins:
 	if (winDR[x][y] == any) {
 	  drwin++;
-	  debugGrid[x][y] = '>';
 	}
       }
-
-      printBoard(debugGrid);
     }
   }
 
-  if (hwin == 3 || vwin == 3 || dlwin == 3 || drwin == 3) return true;
+  // does the player meet the win conditions?
+  if (h0win == 3 || h1win == 3 || h2win == 3
+      || v0win == 3 || v1win == 3|| v2win == 3
+      || dlwin == 3 || drwin == 3) return true;
   else return false;
 }
 
 int emptySpaces(char grid[][3], char none) {
   int empty = 0;
-
-  char debugGrid[][3] {
-    {none, none, none},
-    {none, none, none},
-    {none, none, none}
-  };
   
+  // check how many spots are empty;
   for (int x = 0; x < 3; x++) {
     for (int y = 0; y < 3; y++) {
       char value = grid[x][y];
-      if (value == none) debugGrid[x][y] = '*';
-      else debugGrid[x][y] = 'A';
-
-      printBoard(debugGrid);
-      
+     
       if (value == none) empty++;
     }
   }
@@ -141,6 +167,9 @@ int main() {
       cout << "Welcome to TicTacToe!" << endl;
     }
     else {
+      // print board
+      printBoard(grid);
+      
       // turn message
       if (state == XTURN) {
 	cout << "X's turn" << endl;
@@ -148,33 +177,46 @@ int main() {
       else if (state == OTURN) {
 	cout << "O's turn" << endl;
       }
-      
-      // print board
-      printBoard(grid);
+      // game over message
+      else {
+	cout << "Game Over!" << endl;
+	
+	// draw message
+	if (state == DRAW) {
+	  cout << "it was a draw" << endl;
+	}
+	// x win message
+	else if (state == XWON) {
+	  cout << "X won" << endl;
+	}
+	// o win message
+	else if (state == OWON) {
+	  cout << "O won" << endl;
+	}
+
+	// end game
+	isRunning = false;
+	return 0;
+      }
       
       // get players input
-      char input[2];
+      char input[3];
       cin >> input;
       
-      // convert to grid position
+      // convert input to grid position
       int xpos = int(input[1]) - 48 - 1; // already a number value, but offset down to a 0
       int ypos = int(input[0]) - 96 - 1; // offset char to get number value
       char value = grid[xpos][ypos];
 
-      cout << "input " << int(input[1]) << ", " << int(input[0]) << endl;
-      cout << "converted input " << xpos << ", " << ypos << endl;
-      cout << "empty " << emptySpaces(grid, none) << endl;
-      
       // place into grid
-      if (value != (X || O)) {
-	cout << "Valid move" << endl;
+      if (value == none) {
+	// place X
 	if (state == XTURN) {
-	  cout << "placing x" << endl;
 	  grid[xpos][ypos] = X;
 	  state = OTURN;
 	}
+	// place O
 	else if (state == OTURN) {
-	  cout << "placing o" << endl;
 	  grid[xpos][ypos] = O;
 	  state = XTURN;
 	}
@@ -183,14 +225,15 @@ int main() {
 	cout << "Invalid Move!" << endl;
       }
 
-    
-      // check for a win or draw
+      // check for a draw
       if (emptySpaces(grid, none) == 0) {
 	state = DRAW;
       }
+      // check if X has won
       else if (checkWin(grid, X, any, none) == true) {
 	state = XWON;
       }
+      // check if O has won
       else if (checkWin(grid, O, any, none) == true) {
 	state = OWON;
       }
